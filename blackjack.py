5d2788dbffa2ced0c08fbb6137f083e4bf2e9c9f
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-03-20 12:44:46 +1300

    Added a navigation bar and routes to some new pages for my site

--- blackjack.py
+++ blackjack.py
@@ -1,3 +1,4 @@
+'''Project Start 18/03/2025'''
 from flask import Flask, render_template
 import sqlite3
 
@@ -5,6 +6,31 @@ import sqlite3
 app = Flask(__name__)
 
 
+@app.route('/')  # link with and without the /home will lead home
 @app.route('/home')
 def home():
-    return render_template("home.html", title="Home") 
+    return render_template("home.html", title="Home")
+
+
+@app.route('/play')
+def play():
+    return render_template("play.html", title="Play")
+
+
+@app.route('/stats')
+def stats():
+    return render_template("stats.html", title="Stats")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html", title="About")
+
+
+@app.route('/login')
+def login():
+    return render_template("login.html", title="Login")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)  # make this turned to off when you submit
