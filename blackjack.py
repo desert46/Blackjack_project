1a10392a1250596205c5a9d36ef97b940a151b09
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-23 09:57:58 +1200

    progress on signup page with sql query

--- blackjack.py
+++ blackjack.py
@@ -1,11 +1,11 @@
 '''Project Start 18/03/2025'''
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import hashlib
 import sqlite3
+DATABASE = "blackjack.db"
 
 
 app = Flask(__name__)
-
 
 @app.route('/')  # link with and without the /home will lead home
 @app.route('/home')
@@ -33,10 +33,25 @@ def login():
     return render_template("login.html", title="Login")
 
 
-@app.route('/signup')
+@app.route('/signup', methods=['POST', 'GET'])
 def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = sqlite3.connect(DATABASE)
+        cursor = db.cursor()
+        sql = f'''
+        INSERT INTO player (username, password, xp, level, award_count, 
+        hands_played, wins, losses, pushes, money_wins, money_losses, hits, 
+        busts, stands, dealer_higher, dealer_busts, player_higher)
+        VALUES (?, ?, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+        ''' # sql query to create an account
+        cursor.execute(sql, (username, password))
+        db.commit()   # commits so data is saved
+        db.close()
+        return render_template("signup.html", title="Sign Up")
     return render_template("signup.html", title="Sign Up")
-
+    
 
 @app.route('/settings')
 def settings():
