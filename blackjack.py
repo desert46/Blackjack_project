18bebae42b0030db577a869ce93a7e9cef5dc88e
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-06-11 14:29:25 +1200

    worked with flask session and started on game

--- blackjack.py
+++ blackjack.py
@@ -1,11 +1,17 @@
 '''Project Start 18/03/2025'''
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, session
+from flask_session import Session
 import hashlib
 import sqlite3
 DATABASE = "blackjack.db"
 
 
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+app.secret_key = 'your_secret_key'
+Session(app)
+
 
 @app.route('/')  # link with and without the /home will lead home
 @app.route('/home')
@@ -20,7 +26,15 @@ def play():
 
 @app.route('/stats', methods=['POST', 'GET'])
 def stats():
-    return render_template("stats.html", title="Stats", id=id)
+    if session.get("logged_in"):
+        user_id = session.get("user_id")
+        username = session.get("username")
+        password = session.get("password")
+        flash("You are logged in")
+        return render_template("stats.html", title="Stats", user_id=user_id, username=username, password=password)
+    else:
+        flash("You are not logged in")
+        return render_template("stats.html", title="Stats")
 
 
 @app.route('/about')
@@ -37,12 +51,21 @@ def login():
         print(password)
         db = sqlite3.connect(DATABASE)
         cursor = db.cursor()
-        sql = f'''SELECT id, password FROM Player WHERE username={username}'''
-        print(sql)
-        cursor.execute(sql)
-        id = cursor.fetchone()
-        print(password)
-        print(id)
+        sql = "SELECT id, password FROM Player WHERE username = ?"
+        cursor.execute(sql, (username,))
+        results = cursor.fetchone()
+        if password == results[1]:  # gets password from the results and compares them
+            # gets id if passwords match
+            session['logged_in'] = True
+            session['user_id'] = results[0]
+            session['username'] = username
+            session['password'] = results[1]
+            flash('You were successfully logged in')
+            print("valid")
+            return render_template("play.html", title="Play")
+        else:
+            flash("invalid")
+            return render_template("login.html", title="Login")
     return render_template("login.html", title="Login")
 
 
@@ -53,11 +76,17 @@ def signup():
         password = request.form['password']
         db = sqlite3.connect(DATABASE)
         cursor = db.cursor()
+        # finding duplicate usernames
+        sql = "SELECT * FROM Player WHERE username = ?"
+        cursor.execute(sql, (username,))
+        existing_user = cursor.fetchone()
+        if existing_user:  # checking for duplictae usernames
+            db.close()
+            flash('This username is already taken, try again')
+            return render_template("signup.html", title="Sign Up")
         sql = f'''
-        INSERT INTO Player (username, password, xp, level, award_count,
-        hands_played, wins, losses, pushes, money_wins, money_losses, hits,
-        busts, stands, dealer_higher, dealer_busts, player_higher, money)
-        VALUES (?, ?, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10000)
+        INSERT INTO Player (username, password)
+        VALUES (?, ?)
         '''  # sql query to create an account
         cursor.execute(sql, (username, password))
         db.commit()  # commits so data is saved
