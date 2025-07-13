76917c2195f267835103278aefca29ec72dc24b4
Alex Yao <alexyao@Alexs-MacBook-Pro.local>
2025-07-13 14:53:35 +1200

    finished stats and dashboard and styled some pages and added custom 404

--- blackjack.py
+++ blackjack.py
@@ -22,7 +22,7 @@ def inject_logged_in():
 @app.route('/')  # link with and without the /home will lead home
 @app.route('/home')
 def home():
-    '''This route is for the home page, which is accessible when not logged in.'''
+    '''This route is for the home page, which is accessible when not logged in'''
     if session.get("logged_in"):
         return redirect('/dashboard')
     return render_template("home.html",
@@ -31,30 +31,39 @@ def home():
 
 @app.route('/dashboard')
 def dashboard():
-    '''This route is for the dashboard page, which is only accessible if the user is logged in.'''
-    if session.get("logged_in"):
-        login_msg = "You are logged in as"
-        user_id = session.get("user_id")
-        username = session.get("username")
-        print(username)
-        return render_template("dashboard.html",
-                               title="Dashboard",
-                               user_id=user_id,
-                               username=username,
-                               login_msg=login_msg)
-    else:
-        return render_template("not_logged_in.html",
-                               title="Dashboard",)
+    '''This route is for the dashboard page'''
+    if not session.get("logged_in"):
+        return redirect('/home')
+    user_id = session.get("user_id")
+    username = session.get("username")
+    db = sqlite3.connect(DATABASE)
+    db.row_factory = sqlite3.Row
+    cursor = db.cursor()
+    # finding duplicate usernames
+    sql = "SELECT * FROM Player WHERE id = ?"
+    cursor.execute(sql, (user_id,))
+    stats_data = cursor.fetchone()
+    sql = '''SELECT name, description, image FROM Award WHERE id IN(
+    SELECT aid FROM PlayerAward WHERE pid=?);'''
+    cursor.execute(sql, (user_id,))
+    awards = [dict(row) for row in cursor.fetchall()]
+    db.close()
+    return render_template("dashboard.html",
+                           title="Dashboard",
+                           user_id=user_id,
+                           username=username,
+                           stats_data=stats_data,
+                           awards=awards,)
 
 
 @app.route('/play')
 def play():
     '''This route is for the play page, which is only accessible if the user is logged in.'''
-    if session.get("logged_in"):
-        return render_template("play.html")
-    else:
-        return render_template("not_logged_in.html",
-                               title="Play")
+    if not session.get("logged_in"):
+        flash("Login to play Blackjack")
+        return redirect("/login")
+    return render_template("play.html",
+                           title="Play",)
 
 
 @app.route('/stats', methods=['POST', 'GET'])
@@ -72,11 +81,14 @@ def stats():
         if stats_data is None:
             flash("No Player found with this ID")
         sql = '''SELECT name, description, image FROM Award WHERE id IN(
-        SELECT aid FROM PlayerAward WHERE pid=?'''
+        SELECT aid FROM PlayerAward WHERE pid=?);'''
+        cursor.execute(sql, (searched_id,))
+        awards = [dict(row) for row in cursor.fetchall()]
         db.close()
         return render_template("stats.html", title="Stats",
                                searched_id=searched_id,
-                               stats_data=stats_data,)
+                               stats_data=stats_data,
+                               awards=awards,)
     return render_template("stats.html", title="Stats")
 
 
@@ -112,7 +124,7 @@ def login():
             session['user_id'] = results[0]
             session['username'] = username
             session['password'] = results[1]
-            return render_template("dashboard.html", title="Dashboard")
+            return redirect("/dashboard")
         else:
             flash("Your Username or Password is incorrect")
             return render_template("login.html",
@@ -128,12 +140,27 @@ def signup():
     if session.get("logged_in"):
         return redirect("/dashboard")
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        # Checking the legnth is valid
+        username = request.form['username'].rstrip()
+        password = request.form['password'].rstrip()
+        # Checking the username is valid
         if len(username) < 4 or len(username) > 15:
             flash("Your username must be between 5~15 characters")
             return render_template("signup.html", title="Sign up")
+        elif username.isdigit():
+            flash("Your username must have a letter")
+            return render_template("signup.html", title="Sign up")
+        elif ' ' in username:
+            flash("Your username must not have a space")
+            return render_template("signup.html", title="Sign up")
+        elif "'" in username or '"' in username or ';' in username:
+            flash("Invalid username, please try again")
+        elif '/' in username or '\\' in username or '=' in username:
+            flash("Invalid username, please try again")
+        elif '<' in username or '>' in username:
+            flash("Invalid username, please try again")
+            return render_template("signup.html", title="Sign up")
+
+        # Checking the password is valid
         if len(password) < 6 or len(password) > 15:
             flash("Your password must be between 6~15 characters")
             return render_template("signup.html", title="Sign up")
@@ -155,9 +182,19 @@ def signup():
         INSERT INTO Player (username, password)
         VALUES (?, ?)'''  # sql query to create an account
         cursor.execute(sql, (username, password))
-        db.commit()  # commits so data is saved
+        db.commit()  # account created successfully
+
+        # awarding the player with "Diamond_Create_Account"
+        user_id = cursor.lastrowid
+        sql = '''INSERT INTO PlayerAward (pid, aid) VALUES (?, 1)'''
+        cursor.execute(sql, (user_id,))
+        sql = '''UPDATE Player SET award_count = award_count + 1
+        WHERE id = ?;'''
+        cursor.execute(sql, (user_id,))
+        db.commit()
         db.close()
-        flash("You succesfully created an account, login again to play Blackjack")
+        user_id = None
+        flash("Succesfully created an account, login again to play Blackjack")
         return render_template("login.html",
                                title="Login")
     return render_template("signup.html",
@@ -167,6 +204,9 @@ def signup():
 @app.route('/settings')
 def settings():
     '''This route is for the settings page, which allows users to change their account settings.'''
+    if not session.get("logged_in"):
+        return render_template("not_logged_in.html",
+                               title="Settings")
     return render_template("settings.html",
                            title="Settings")
 
@@ -174,9 +214,17 @@ def settings():
 @app.route('/log_out')
 def logout():
     '''This route is for logging out the user, clearing the session data.'''
+    if not session.get("logged_in"):
+        return redirect('/home')
     session.clear()
     return render_template("log_out.html",
                            title="Logged Out")
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    '''custom 404 page not found page'''
+    return render_template("404.html", title="Page Not Found"), 404
 
 
 if __name__ == "__main__":
